8858e4c935dae907d71cff136e20441dc0c011ed
Aastin-Wilson <aastinwilson@gmail.com>
2025-04-14 23:52:09 +0530

    created half of home page and then created category page which is not yet started

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
         (
             <div className="relative group">
                 <img src={assets.profile_icon} className="w-10" alt="" />
-                <ul className="hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-g text-sm z-40">
+                <ul className="hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-lg text-sm z-40">
                     <li onClick={()=>navigate("my-orders")} className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer">My Orders</li>
                     <li onClick={logout} className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer">Logout</li>
                 </ul>
